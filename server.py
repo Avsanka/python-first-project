d6c7ff372fe583b73b9263212973ce38daf36977
Avsanka <nikita555534@gmail.com>
2018-10-19 20:14:02 +0700

    We made mnogo-mnogo-mnogo-mnogo all

--- server.py
+++ server.py
@@ -1,21 +1,32 @@
 from flask import Flask, url_for, render_template
+
 app = Flask(__name__)
+
+
+def generate_links():
+    with app.test_request_context():
+        pinkblacklink = url_for('hello_user', username='Dave Grohl')
+        KurtCobain_the_Great_link = url_for('hello_user', username='Kurt Cobain The Great')
+        index_link = url_for('index')
+        links = (
+            (" Dave Grohl", pinkblacklink),
+            (" Kurt Cobain", KurtCobain_the_Great_link),
+            (" Krist Novoselic", index_link),
+        )
+    return links
 
 
 @app.route('/')
 def index():
-   with app.test_request_context():
-        link = url_for('hello_user', username='Pink Black')
-
-
-   return render_template('index.html', link=link)
+   links = generate_links()
+   return render_template('index.html', links=links)
 
 
 @app.route('/user/')
 @app.route('/user/<username>')
 def hello_user(username=None):
-    numbers = [1,2,3,4,5,6,7,8,9,0]
-    return render_template('user.html', username=username, numbers=numbers)
+    links=generate_links()
+    return render_template('user.html', username=username,)
 
 
 with app.test_request_context():
