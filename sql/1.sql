e37c776debc07cfd85642b88e7386bd1e24e8643
Avsanka <nikita555534@gmail.com>
2018-11-22 20:28:24 +0700

    nicht arbeite

--- sql/1.sql
+++ sql/1.sql
@@ -4,5 +4,28 @@ CREATE TABLE `product` (
   `price` int(10) unsigned DEFAULT NULL,
   `description` varchar(255) DEFAULT NULL,
   `category_id` int(4) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`));
+
+CREATE TABLE `category` (
+  `id` int(4) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `image_url` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`));
+
+ALTER TABLE `product`
+ADD INDEX `FK_category_idx` (`category_id` ASC);
+;
+ALTER TABLE `shop`.`product`
+ADD CONSTRAINT `FK_category`
+  FOREIGN KEY (`category_id`)
+  REFERENCES `shop`.`category` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+
+ CREATE TABLE `image` (
+  `id` int(4) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL;
+  PRIMARY KEY (`id`));
 
