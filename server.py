b115f67e5283759f9a040f03105fc1b602cdfad6
Avsanka <nikita555534@gmail.com>
2018-12-20 19:59:16 +0700

    341234124112412412

--- server.py
+++ server.py
@@ -1,25 +1,8 @@
 from flask import Flask, url_for, render_template
+from repository import shop
 
 app = Flask(__name__)
 
-import pymysql
-
-connection = pymysql.connect(
-host='192.168.33.10',
-user='remote_user',
-password='123123',
-db='shop',
-charset='utf8mb4',
-cursorclass=pymysql.cursors.DictCursor
-)
-
-@app.route('/test/')
-def test():
-    cursor = connection.cursor
-    cursor.execute('SELECT * FROM category')
-    result = cursor.fetchall()
-
-    return str(result[0])
 
 def generate_links():
     with app.test_request_context():
@@ -47,10 +30,35 @@ def generate_links():
 
     return links
 
+def convert(x):
+    return{
+        'category_id': x.get('id'),
+        'heading': x.get('title'),
+        'image_title': x.get('title'),
+        'subheading': x.get('description'),
+        'image.src': x.get('image_url')
+    }
+
 
 @app.route('/')
 def index():
     links = generate_links()
+
+    categories = shop.find_all_categories()
+    slides = list()
+    for x in categories:
+        slide = {
+        'category_id': x.get('id'),
+        'heading': x.get('title'),
+        'image_title': x.get('title'),
+        'subheading': x.get('description'),
+        'image_src': x.get('image_url')
+    }
+    slides.append(slide)
+
+    return render_template('index.html', links=links, slides=slides)
+
+
     slides = [
         {
             'image_src': 'https://bipbap.ru/wp-content/uploads/2017/04/0_7c779_5df17311_orig.jpg',
@@ -90,6 +98,19 @@ def hello_user(username=None):
         links=links,
     )
 
+@app.route('/category/<int:category_id>')
+def category_page(category_id):
+    links = generate_links()
+
+    category = shop.find_category(category_id)
+    products = shop.find_products_by_category(category_id)
+
+    return render_template(
+        'category.html',
+        products=products,
+        category=category,
+        links=links,
+    )
 
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=True)
